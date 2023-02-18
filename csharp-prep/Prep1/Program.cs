78b7acd1a317f6be4a92f3221a829341a6989a8e
SamanthaSortomme <sor23001@byui.edu>
2023-02-18 12:50:53 -0800

    #1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first;
+        string last;
+        //Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        last = Console.ReadLine();
+        Console.Write($"\nYour name is {last}, {first} {last}.");
     }
 }
