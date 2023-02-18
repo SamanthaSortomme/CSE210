124d4d0b768f5fad4aba94e5d67f6c76f4cae181
SamanthaSortomme <sor23001@byui.edu>
2023-02-18 15:12:47 -0800

    #3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int numRange = 25;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, numRange + 1);
+        int guess;
+        int guessCount = 0;
+        string cont = "yes";
+        do
+        {
+            Console.Write($"pick a number between 0 and {numRange}: ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low Joe.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high fry.");
+            }
+            else
+            {
+                Console.WriteLine($"You win, Jinn! only took you {guessCount} guesses! \nWant to play again?(yes/no) ");
+                cont = Console.ReadLine();
+                guessCount = 0;
+            }
+        } while (cont == "yes");
+        Console.WriteLine("Good bye.");
     }
 }
