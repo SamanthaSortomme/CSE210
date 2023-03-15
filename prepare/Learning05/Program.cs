da03b1eb988da983bdb482091efc24336c8b2a1a
SamanthaSortomme <sor23001@byui.edu>
2023-03-14 17:59:05 -0700

    Week 05 learning finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square(12.5, "blue"));
+        shapes.Add(new Rectangle(17.442, 11.81, "yellow"));
+        shapes.Add(new Circle(54, "purple"));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.Write($"Color: {shape.GetColor()},  Area: {shape.GetArea()}\n");
+        }
     }
 }
